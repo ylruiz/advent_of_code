1787e3e65e8e1fe77a308bae3d9a553380d3fce8
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2024-12-13 22:09:57 +0100

    Add 05.12.2024 second puzzle solution

--- 2024/day_05/solution.ts
+++ 2024/day_05/solution.ts
@@ -1,19 +1,21 @@
 import { readFileSync } from "fs";
 
-// const filePath = "../inputs/05_12_2024.txt";
-const filePath = "../inputs/test.txt";
+const filePath = "../inputs/05_12_2024.txt";
+// const filePath = "../inputs/test.txt";
 
-let pageUpdates: number[][];
-let orderedUpdates: number[][];
-let totalCurrectMiddlePageNumber = 0;
+let pageUpdates = new Map<number, number[]>();
+let orderedUpdates: number[][] = [];
 
 function processData() {
   const rowData = readFileSync(filePath, "utf-8");
 
-  pageUpdates = rowData
+  rowData
     .split("\n")
     .filter((line) => line.includes("|"))
-    .map((line) => line.split("|").map(Number));
+    .map((line) => {
+      const pagePair = line.split("|").map(Number);
+      addConnection(pagePair[0], pagePair[1]);
+    });
 
   orderedUpdates = rowData
     .split("\n")
@@ -21,41 +23,99 @@ function processData() {
     .map((line) => line.split(",").map(Number));
 }
 
-function sumMiddlePageNumber(isSummingInvalids?: boolean) {
-  processData();
+processData();
 
-  for (let i = 0; i < orderedUpdates.length; i++) {
-    let isOrdered = true;
-    let j = 0;
-    let invalidPagesIndexes: number[] = [];
-
-    while (j < pageUpdates.length && isOrdered) {
-      const x = pageUpdates[j][0];
-      const xIndex = orderedUpdates[i].indexOf(x);
-      const isXFound = xIndex >= 0;
-
-      const y = pageUpdates[j][1];
-      const yIndex = orderedUpdates[i].indexOf(y);
-      const isYFound = yIndex >= 0;
-
-      if (isXFound && isYFound) {
-        isOrdered = xIndex < yIndex;
-      } else {
-        isOrdered = true;
-      }
-
-      j++;
-    }
-
-    if (isOrdered) {
-      const middleIndex =
-        parseInt((orderedUpdates[i].length / 2).toFixed(0)) - 1;
-      totalCurrectMiddlePageNumber += orderedUpdates[i][middleIndex];
-    }
+function addConnection(x: number, y: number) {
+  if (pageUpdates.has(x)) {
+    pageUpdates.get(x)?.push(y);
+  } else {
+    pageUpdates.set(x, [y]);
   }
-
-  console.log(totalCurrectMiddlePageNumber);
-  return totalCurrectMiddlePageNumber;
 }
 
-sumMiddlePageNumber();
+function insertElemUpdate(
+  currentElement: number,
+  seenElements: number[],
+  rules: Map<number, number[]>
+) {
+  let flag = false;
+  for (let i = 0; i < seenElements.length; i++) {
+    if (flag) {
+      break;
+    }
+    if (rules.has(currentElement)) {
+      const connections = rules.get(currentElement);
+      if (connections?.includes(seenElements[i])) {
+        seenElements.splice(i, 0, currentElement);
+        flag = true;
+      }
+    }
+  }
+  if (!flag) {
+    seenElements.push(currentElement);
+  }
+}
+
+function isUpdateOrdered(update: number[], rules: Map<number, number[]>) {
+  let seenElements: number[] = [];
+  seenElements.push(update[0]);
+  for (let i = 1; i < update.length; i++) {
+    const u = update[i];
+    if (rules.has(u)) {
+      const connections = rules.get(u);
+      for (let l = 0; l < seenElements.length; l++) {
+        if (connections!.includes(seenElements[l])) {
+          return false;
+        }
+      }
+      seenElements.push(u);
+    } else {
+      seenElements.push(u);
+    }
+  }
+  return true;
+}
+
+function orderUpdate(update: number[], rules: Map<number, number[]>) {
+  let seenElements: number[] = [];
+  seenElements.push(update[0]);
+  for (let i = 1; i < update.length; i++) {
+    let u = update[i];
+    if (rules.has(u)) {
+      insertElemUpdate(u, seenElements, rules);
+    } else {
+      seenElements.push(u);
+    }
+  }
+  const index = Math.ceil(seenElements.length / 2) - 1;
+  return seenElements[index];
+}
+
+function getUpdates(
+  fullData: number[][],
+  rules: Map<number, number[]>,
+  ordered: boolean
+) {
+  function filterFun(data: number[]) {
+    return ordered
+      ? isUpdateOrdered(data, rules)
+      : !isUpdateOrdered(data, rules);
+  }
+  return fullData.filter(filterFun);
+}
+
+const resultsValid: number[] = getUpdates(
+  orderedUpdates,
+  pageUpdates,
+  true
+).map((subArray) => subArray[Math.ceil(subArray.length / 2) - 1]);
+const totalValid = resultsValid.reduce((sum, current) => sum + current, 0);
+console.log(totalValid);
+
+const resultsInvalid: number[] = getUpdates(
+  orderedUpdates,
+  pageUpdates,
+  false
+).map((subArray) => orderUpdate(subArray, pageUpdates));
+const totalInvalid = resultsInvalid.reduce((sum, current) => sum + current, 0);
+console.log(totalInvalid);
