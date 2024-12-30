8050c6a10d69ba8ed0b40ea85e78b5f4e5c129ab
Raimel A. Medina <medinaramosar1992@gmail.com>
2024-12-30 19:52:45 +0100

    add solution day_11 part 2

--- 2024/day_11/solution.ts
+++ 2024/day_11/solution.ts
@@ -1,80 +1,92 @@
-// Rules of the stone evolution in time
-// * 0 -> 1
-// * If even number of digits -> first half to the left & second half to the right. We have to take
-//   into account that the string must correspond to a valid number
-// * If none of the above then multiply corresponding number by 2024
-
 import { readFileSync } from "fs";
+import * as path from "path";
 
-const filePath = "../inputs/11_12_2024.txt";
-// const filePath = "../inputs/test.txt";
+const filePath = path.join(__dirname, "../../inputs/11_12_2024.txt");
+// const filePath = path.join(__dirname, "../../inputs/test.txt");
 
 let stones: number[] = [];
+let stonesMap = new Map<number, number>();
 
 function processData() {
   try {
     const rowData = readFileSync(filePath, "utf-8");
     stones = rowData.trim().split(" ").map(Number);
+    for (const element of stones) {
+      if (stonesMap.has(element)) {
+        const val = stonesMap.get(element) ?? 0;
+        stonesMap.set(element, val + 1);
+      } else {
+        stonesMap.set(element, 1);
+      }
+    }
   } catch (err) {
     console.log(`Error reading filePath: ${filePath}`);
   }
 }
 
 processData();
+console.log(stonesMap);
 
-function evolveStone(index: number, stoneData: number[]) {
-  if (stoneData[index] === 0) {
-    stoneData[index] = 1;
-    return 0;
-  }
+// Rules of the stone evolution in time
+// * 0 -> 1
+// * If even number of digits -> first half to the left & second half to the right. We have to take
+//   into account that the string must correspond to a valid number
+// * If none of the above then multiply corresponding number by 2024
+function evolveStone(stone: number, count: number, stoneRep: Map<number, number>) {
+    if (stone === 0) {
+        stoneRep.set(1, (stoneRep.get(1) ?? 0) + count);
+        return;
+    }
 
-  // [23, 2]
-  // index = 0 => [2, 3, 2]
-  //
-  let tempStone = stoneData[index].toString();
-  if (tempStone.length % 2 === 0) {
-    const leftStone = parseInt(tempStone.substring(0, tempStone.length / 2));
-    const rightStone = parseInt(
-      tempStone.substring(tempStone.length / 2, tempStone.length)
-    );
+    let tempStone = stone.toString();
+    if (tempStone.length % 2 === 0) {
+        const leftStone = parseInt(tempStone.substring(0, tempStone.length / 2));
+        const rightStone = parseInt(tempStone.substring(tempStone.length / 2, tempStone.length));
+        stoneRep.set(leftStone, (stoneRep.get(leftStone) ?? 0) + count);
+        stoneRep.set(rightStone, (stoneRep.get(rightStone) ?? 0) + count);
+        return;
+    }
 
-    stoneData.splice(index, 1, leftStone, rightStone);
-    return 1;
-  }
-
-  stoneData[index] *= 2024;
-  return 0;
+    const newStone = 2024 * stone;
+    stoneRep.set(newStone, (stoneRep.get(newStone) ?? 0) + count);
+    return;
 }
 
-function evolveAllStones(stoneData: number[]) {
-  let index = 0;
+function evolveAllStones(stonesData: Map<number, number>) {
+  let tempMap = new Map<number, number>();
 
-  while (index < stoneData.length) {
-    const shift = evolveStone(index, stoneData);
-    index = index + 1 + shift;
+  for (const stone of stonesData) {
+    evolveStone(stone[0], stone[1], tempMap);
   }
+  return tempMap;
 }
 
-function calculateTotalStones(stoneData: number[], timeSteps: number) {
+function calculateTotalStones(
+  stoneData: Map<number, number>,
+  timeSteps: number
+) {
+  let currentMap = new Map(stoneData);
   for (let t = 0; t < timeSteps; t++) {
-    evolveAllStones(stoneData);
-    // console.log(`Finish with t = ${t}`);
+    currentMap = evolveAllStones(currentMap);
   }
-  return stoneData.length;
+  let totalStones = 0;
+  currentMap.forEach((value) => (totalStones += value));
+  return totalStones;
 }
 
 console.log(stones);
+console.log(calculateTotalStones(stonesMap, 75));
+
 const startTime = performance.now();
 const beforeMemory = process.memoryUsage().heapUsed;
-calculateTotalStones(stones, 30);
+
 const afterMemory = process.memoryUsage().heapUsed;
 console.log(`Memory used: ${(afterMemory - beforeMemory) / 1024} KB`);
 
-// The total distance should be equal to 2430334
 const endTime = performance.now();
 const timeTaken = (endTime - startTime)/1000;
 console.log(
-  `Time taken by calculateTotalDistance: ${timeTaken.toFixed(4)} seconds`
+  `Time taken by calculate total number of stones: ${timeTaken.toFixed(4)} seconds`
 );
 
 console.log(stones.length);
